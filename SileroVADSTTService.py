e9b8810aa314a06215698e2e8fa947160a7a99a6
allenmylath <allengeorgemylath@gmail.com>
2024-12-13 16:45:58 +0530

    Update SileroVADSTTService.py

--- SileroVADSTTService.py
+++ SileroVADSTTService.py
@@ -6,6 +6,7 @@ import importlib.resources
 from typing import Tuple
 
 from pipecat.services.ai_services import STTService
+from pipecat.frames.frames import ErrorFrame, Frame, TranscriptionFrame, AudioRawFrame
 
 class SileroVADSTTService(STTService):
     """STTService that uses Silero VAD (ONNX) for speech detection before running
